292c47b49daf7c8ee4fb2504d516df6b2fa51b25
Myriam <myriam.azzimani@epitech.eu>
2019-02-09 16:06:19 +0100

    fichier myriam my_wysiwyg.js changement a lire avc sont fichier my_wysiwyg.html

--- my_wysiwyg.js
+++ my_wysiwyg.js
@@ -1,1 +1,35 @@
-$('p').css('color','blue');
+
+(function ( $ ) {
+ 
+    $.fn.my_wysiwyg = function( options ) {
+
+        $.fn.italique = function(name){
+            this.append('<button>'+ name +'</button>');
+        } 
+        $('body').prepend('<div></div>');
+        $('div').italique('i');
+        $('div').italique('G');
+ 
+        var settings = $.extend({
+            color: "black",
+            backgroundColor: "white"
+        }, options );
+ 
+       /* this.css({
+            color: settings.color,
+            backgroundColor: settings.backgroundColor
+        });*/
+        this.attr({
+            style:settings.style
+        });
+
+    };
+    
+ 
+}( jQuery ));
+
+$( "textarea" ).my_wysiwyg({ 
+    style: 'width:800px; height:100px;'
+    
+       
+});
